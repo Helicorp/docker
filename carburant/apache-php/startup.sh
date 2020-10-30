abd46ce92af28516f9e2a7fb44fa51642fa7eec9
Helicraft08 <53180119+Helicraft08@users.noreply.github.com>
2020-10-30 19:29:25 +0100

    Update startup.sh

--- carburant/apache-php/startup.sh
+++ carburant/apache-php/startup.sh
@@ -42,7 +42,7 @@ while sleep 20; do
   PROCESS_3_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 || $PROCESS_2_STATUS -ne 0 || $PROCESS_3_STATUS -ne 0]; then
+  if [ $PROCESS_1_STATUS -ne '0' ]  || [ $PROCESS_2_STATUS -ne '0' ] || [ $PROCESS_3_STATUS -ne '0' ]; then
     echo "Un processus a crash."
     exit 1
   fi
