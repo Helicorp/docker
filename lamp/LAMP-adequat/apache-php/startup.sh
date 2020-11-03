c6aa237390a4681a2d5d2b5ce6824df34cad403a
Helicraft08 <53180119+Helicraft08@users.noreply.github.com>
2020-11-03 12:18:06 +0100

    Update startup.sh

--- lamp/LAMP-adequat/apache-php/startup.sh
+++ lamp/LAMP-adequat/apache-php/startup.sh
@@ -1,4 +1,4 @@
- #! /bin/bash
+#! /bin/bash
 
 #demarrage du service apache2
 service apache2 start
@@ -34,16 +34,16 @@ then
 fi 
 
 while sleep 20; do
-  ps aux |grep apache2 |grep -q -v grep
+  service apache2 status
   PROCESS_1_STATUS=$?
-  ps aux |grep vsftpd |grep -q -v grep
+  service vsftpd status
   PROCESS_2_STATUS=$?
-  ps aux |grep cron |grep -q -v grep
+  service cron status
   PROCESS_3_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 $PROCESS_3_STATUS -ne 0 -o ]; then
-    echo "One of the processes has already exited."
+  if [ $PROCESS_1_STATUS -ne '0' ]  || [ $PROCESS_2_STATUS -ne '0' ] || [ $PROCESS_3_STATUS -ne '0' ]; then
+    echo "Un processus a crash."
     exit 1
   fi
 done
