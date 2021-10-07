10e0c29210518f81256930ec33494e10369d2068
Helicraft08 <53180119+Helicraft08@users.noreply.github.com>
2021-10-07 11:27:00 +0200

    mise à jour avec les dernières informations de la VM

--- lamp/LAMP-adequat/apache-php/startup.sh
+++ lamp/LAMP-adequat/apache-php/startup.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+ #! /bin/bash
 
 #demarrage du service apache2
 service apache2 start
@@ -34,18 +34,16 @@ then
 fi 
 
 while sleep 20; do
-  service apache2 status
+  ps aux |grep apache2 |grep -q -v grep
   PROCESS_1_STATUS=$?
-  service vsftpd status
+  ps aux |grep vsftpd |grep -q -v grep
   PROCESS_2_STATUS=$?
-  service cron status
+  ps aux |grep cron |grep -q -v grep
   PROCESS_3_STATUS=$?
   # If the greps above find anything, they exit with 0 status
   # If they are not both 0, then something is wrong
-  if [ $PROCESS_1_STATUS -ne '0' ]  || [ $PROCESS_2_STATUS -ne '0' ] || [ $PROCESS_3_STATUS -ne '0' ]; then
-    echo "Un processus a crash."
+  if [ $PROCESS_1_STATUS -ne 0 -o $PROCESS_2_STATUS -ne 0 $PROCESS_3_STATUS -ne 0 -o ]; then
+    echo "One of the processes has already exited."
     exit 1
   fi
 done
-
-# <!--------------------Just Monika-----------------------!>
