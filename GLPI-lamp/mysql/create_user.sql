c6aaf0bae1f2ffa11ee293e612f06d1fe3bd427b
Helicorp <idriss.benahmed@gmail.com>
2022-06-21 16:35:37 +0200

    Update de GLPI

--- GLPI-lamp/mysql/create_user.sql
+++
@@ -1,4 +0,0 @@
-CREATE USER 'admin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
-CREATE USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
